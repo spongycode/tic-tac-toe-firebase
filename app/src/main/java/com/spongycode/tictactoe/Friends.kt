b1b9d32727b5929f3cf647d16174cfbcd67faf15
spongycode <hkbettiah@gmail.com>
2021-02-15 11:46:46 +0530

    Fecthed all Users in Tab Fragment (RView) and send play request to other players

--- app/src/main/java/com/spongycode/tictactoe/Friends.kt
+++ app/src/main/java/com/spongycode/tictactoe/Friends.kt
@@ -5,14 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_friends.*
 
-class Friends: Fragment() {
+class Friends : Fragment() {
+
+    val firestore = FirebaseFirestore.getInstance()
+    private var linearLayoutManager: LinearLayoutManager? = null
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
+        // rv in tab fragment for all users init
+        firestore.collection("users").get()
+                .addOnSuccessListener { documents ->
+                    try {
+                        val userList = mutableListOf<UserDataClass>()
+                        for (document in documents) {
+                            val user = document.toObject(UserDataClass::class.java)
+                            userList.add(user)
+                        }
+                        linearLayoutManager = LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false)
+                        rv_users?.layoutManager = linearLayoutManager
+                        rv_users?.adapter = UserAdapter(userList, requireContext(), firestore) // Your adapter
+                        rv_users?.setHasFixedSize(true);
+
+                    } catch (ex: Exception) {
+                        android.widget.Toast.makeText(requireContext(), ex.toString(), android.widget.Toast.LENGTH_SHORT)
+                                .show()
+                    }
+
+
+                }
+        // rv in tab fragment for all end
+
         // Inflating layout All Users Fragment
         return inflater.inflate(R.layout.fragment_friends, container, false)
+
     }
 }
---
+++ app/src/main/java/com/spongycode/tictactoe/UserAdapter.kt
@@ -0,0 +1,110 @@
+package com.spongycode.tictactoe
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_home.*
+
+val firestore = FirebaseFirestore.getInstance()
+val auth = FirebaseAuth.getInstance()
+
+
+class UserAdapter(
+    private val userList: MutableList<UserDataClass>,
+    private val context: Context,
+    private val firestoreDB: FirebaseFirestore
+) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.user_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = userList[position]
+        holder.fname.text = user.fname
+        holder.lname.text = user.lname
+
+        if (user.imageurl!="") {
+
+        Glide.with(context).load(user.imageurl).into(holder.imageurl)
+        }
+
+        val btn_battle = holder.itemView.findViewById<Button>(R.id.btn_battle)
+        if (user.userid == auth.currentUser?.uid.toString()) {
+            btn_battle.setText("Profile")
+        } else {
+
+            btn_battle.setOnClickListener {
+                firestore.collection("users")
+                    .whereEqualTo("userid", auth.currentUser?.uid.toString())
+                    .get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            for (data in task.result!!) {
+                                val fname = data.toObject(UserDataClass::class.java).fname
+                                val lname = data.toObject(UserDataClass::class.java).lname
+                                val imageurl = data.toObject(UserDataClass::class.java).imageurl
+                                firestore.collection("users")
+                                    .document(user.userid)
+                                    .collection("pendingrequest")
+                                    .document(auth.currentUser?.uid.toString())
+
+                                       //set function is being updated now, earlier it was "opponentid" <14-02> ,,,,now "userid"<15-02>
+                                    .set(
+                                        hashMapOf(
+                                            "fname" to fname,
+                                            "lname" to lname,
+                                            "imageurl" to imageurl,
+                                            "userid" to auth.currentUser?.uid.toString()
+
+                                        )
+                                    )
+                                    .addOnSuccessListener {
+                                        Toast.makeText(
+                                            context,
+                                            "Challenge Sent to $fname $lname",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(
+                                            context,
+                                            "Failed Sending Challenge",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+
+
+                            }
+                        } else {
+
+                        }
+                    }
+
+
+            }
+
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    inner class ViewHolder internal constructor(view: View) : RecyclerView.ViewHolder(view) {
+        internal var fname: TextView = view.findViewById(R.id.user_fname)
+        internal var lname: TextView = view.findViewById(R.id.user_lname)
+        internal var imageurl: ImageView = view.findViewById(R.id.user_imageurl)
+    }
+}
