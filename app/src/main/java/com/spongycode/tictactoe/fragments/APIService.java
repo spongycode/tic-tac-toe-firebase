59c4a9130200355220170c7411d8405063025346
spongycode <hkbettiah@gmail.com>
2021-04-29 14:29:48 +0530

    Notification -> bug fix

--- app/src/main/java/com/spongycode/tictactoe/fragments/APIService.java
+++ app/src/main/java/com/spongycode/tictactoe/fragments/APIService.java
@@ -11,7 +11,7 @@ import retrofit2.http.POST;
 public interface APIService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAAwhY0LpI:APA91bFRoy9XCuT0yGtweg1ia39bZ-We0YAK0aTixxf9rCO1Kc9AULXrGwLFhzS4fgi5G_e8hzOy4v6IWB1m5b5F9ACAILb1f8mTYpRqt9XZLgsg_r0Nppj65ss9qw4bx5yimME8_k6m"
+            "Authorization:key=AAAAwhY0LpI:APA91bGRoWE6dN1rVt7D4XxCuAN_rQzCUgPVfTh2pFytjjuOIGhQnRbPhxJ3d5DMWRmMDak5f5ySB3kH8t-XyGiXnL0djS__LWE0OfCd3z2Acy_2CdfuxH9l7tN817vrwYvDtX5Zxrns"
     })
     @POST("fcm/send")
     Call<MyResponse> sendNotification(@Body Sender body);
